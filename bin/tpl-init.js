50b47b3f9eb4acdfe16f60e6a8b99dff63072e81
N2ET <1766871551@qq.com>
2017-03-09 22:15:22 +0800

    modify ask

---
+++ bin/tpl-init.js
@@ -0,0 +1,19 @@
+const R_program = require('commander')
+
+const R_logger = require('../lib/logger')
+const R_config = require('../lib/config')
+const R_init = require('../lib/init')
+
+R_program
+  .usage('[owner] [project-name]')
+  .option('--debug', 'log debug info')
+
+R_program.parse(process.argv)
+
+console.log()
+process.on('exit', function () {
+  console.log()
+})
+
+R_config.parse(R_program)
+R_init.init()
